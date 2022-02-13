49a3ce79d3c7b403b96418d137ea83185b708a7d
Mihkel Tiganik <mtiganik@gmail.com>
2022-02-13 20:47:25 +0200

    capitalize letters

--- ViewModel.js
+++ ViewModel.js
@@ -1,7 +1,15 @@
-function AppViewModel(){
-    this.firstName ="Bert";
-    this.lastName = "Bertington";
-}
+function AppViewModel() {
+    this.firstName = ko.observable("Mihkel");
+    this.lastName = ko.observable("Tiganik");
 
+    this.fullName = ko.computed(function(){
+        return this.firstName() + " " + this.lastName();
+    }, this);
+
+    this.capitalizeLastName = function(){
+        var currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
+    };
+}
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
