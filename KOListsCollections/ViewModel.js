e54874994699baee3b0b973c5873980ce2859a36
Mihkel Tiganik <mtiganik@gmail.com>
2022-02-15 22:32:03 +0200

    DataArray responsive features

---
+++ KOListsCollections/ViewModel.js
@@ -0,0 +1,36 @@
+
+// Class to represent row in the seat reservation grid
+function SeatReservation(name, initialMeal){
+    var self = this;
+    self.name = name;
+    self.meal = ko.observable(initialMeal);
+
+    self.formattedPrice = ko.computed(function(){
+        var price = self.meal().price;
+        return price ? '$' + price.toFixed(2) : "None";
+    });
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationViewModel(){
+    var self = this;
+
+    // Non-editable catalog data - would come from the server
+    self.availableMeals = [
+        { mealName: "Standard (sandwich)", price: 0},
+        { mealName: "Premium (Roasted chickpeas)", price: 34.95 },
+        { mealName: "Ultimate (quinoa dumplings)", price: 290 }
+    ];
+
+    // Editable data
+    self.seats = ko.observableArray([
+        new SeatReservation("Mihkel", self.availableMeals[1]),
+        new SeatReservation("Anngrett", self.availableMeals[2])
+    ]);
+
+    self.addSeat= function(){
+        self.seats.push(new SeatReservation("",self.availableMeals[0]));
+    }
+}
+ko.applyBindings(new ReservationViewModel());
+
