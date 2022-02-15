1c886b9dd75fbca1ebc3ba36fd8073faa0d92b12
Mihkel Tiganik <mtiganik@gmail.com>
2022-02-16 00:55:44 +0200

    SPA Step 2 no db for myself

--- KOSPA/ViewModel.js
+++ KOSPA/ViewModel.js
@@ -2,6 +2,18 @@ function WebmailViewModel(){
     // Data
     var self = this;
     self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+    self.chosenFolderId = ko.observable();
+    self.chosenFolderData = ko.observable();
+
+    // Behaviours
+    self.goToFolder = function(folder){ 
+        self.chosenFolderId(folder); 
+
+        $.get('/mail', {folder:folder}, self.chosenFolderData);
+    };
+    self.goToFolder('Inbox');
+
+
 };
 
 ko.applyBindings(new WebmailViewModel());
